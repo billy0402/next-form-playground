459123245069ea12934a9f0cc5f5a2ab75cdca77
Billy Huang <10646003@ntub.edu.tw>
2022-01-20 13:01:46 +0800

    feat(UserForm): generate devtools with quickstart

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,24 +1,30 @@
 import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
 
 import { User } from '@models/User';
 
 const UserForm = () => {
   const {
     register,
+    control,
     handleSubmit,
     formState: { errors },
   } = useForm<User>();
   const onSubmit = (data: User) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('firstName')} />
-      <input {...register('lastName', { required: true })} />
-      {errors.lastName && <p>Last name is required.</p>}
-      <input {...register('age', { pattern: /\d+/ })} />
-      {errors.age && <p>Please enter number for age.</p>}
-      <input type='submit' />
-    </form>
+    <>
+      <DevTool control={control} placement='top-right' />
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('firstName')} />
+        <input {...register('lastName', { required: true })} />
+        {errors.lastName && <p>Last name is required.</p>}
+        <input {...register('age', { pattern: /\d+/ })} />
+        {errors.age && <p>Please enter number for age.</p>}
+        <input type='submit' />
+      </form>
+    </>
   );
 };
 
